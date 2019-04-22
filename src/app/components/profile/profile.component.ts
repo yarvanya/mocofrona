b027ea5f62fa809841e69721562d446e4ca47baa
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2019-04-22 21:11:09 +0300

    Deleted test image from the project

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ProfileInterface } from '../../interfaces/profile-interface';
@@ -21,9 +21,14 @@ export class ProfileComponent implements OnInit {
   profileData: any;
   defaultFieldValue: string = 'Fill this field in';
 
+  fileToUpload: File = null;
+
+  @ViewChild("fileInput") fileInput;
+
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
+    avatar: new FormControl(''),
     country: new FormControl(''),
     city: new FormControl(''),
     address: new FormControl(''),
@@ -51,6 +56,7 @@ export class ProfileComponent implements OnInit {
       this.profileForm.setValue({
         firstName: this.profileData.firstName,
         lastName: this.profileData.lastName,
+        avatar: this.profileData.avatar,
         country: this.profileData.country,
         city: this.profileData.city,
         address: this.profileData.address,
@@ -69,10 +75,10 @@ export class ProfileComponent implements OnInit {
     this.updateProfileData().subscribe(profile_data => {
       this.profileData = profile_data.profile;
       this.showToastr(profile_data);
-      this.modalRef.hide();
+      this.modalRef && this.modalRef.hide();
     }, catchedError => {
       this.showToastr(catchedError.error);
-      this.modalRef.hide();
+      this.modalRef && this.modalRef.hide();
     });
   }
 
@@ -87,6 +93,21 @@ export class ProfileComponent implements OnInit {
       `${constants.backEndUrl}${constants.routes.get_profile}/${this.authData.profileId}`,
       this.profileForm.value
     );
+  }
+
+  fileChange(file: FileList) {
+    this.fileToUpload = file.item(0);
+
+    const reader = new FileReader();
+    reader.readAsDataURL(this.fileToUpload);
+    reader.onload = (event: any) => {
+      this.profileForm.controls['avatar'].setValue(event.target.result);
+      this.profileForm.controls['avatar'].value && this.updateProfile();
+    }
+  }
+
+  getImageSrc() {
+    return this.profileForm.controls['avatar'].value;
   }
 
   showToastr(data) {
